5c210318dbb72d1c73cb8c579ebbd4ce3e5c657f
NikithaKurremla <kurremlanikitha@gmail.com>
2025-05-06 14:27:26 +0530

    all

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate=require("ejs-mate");
-
-
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError =require("./utils/ExpressError.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -34,11 +34,11 @@ app.get("/", (req, res) => {
 });
 
 //Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings",  wrapAsync(async (req, res) => {
   const allListings = await Listing.find({});
   res.render("listings/index.ejs", { allListings });
   // console.log(allListings);
-});
+}));
 
 //New Route
 app.get("/listings/new", (req, res) => {
@@ -46,46 +46,81 @@ app.get("/listings/new", (req, res) => {
 });
 
 //Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync( async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
+  // console.log(listing);
   res.render("listings/show.ejs", { listing });
-});
+}));
 
 //Create Route
-app.post("/listings", async (req, res) => {
-  const listingData = new Listing(req.body.listing);
-  listingData.image = {
-    filename: "listingimage",
-    url: listingData.imageUrl
-  };
-  delete listingData.imageUrl;
-  const newListing = new Listing(listingData);
-  await newListing.save();
-  res.redirect("/listings");
-});
+// app.post("/listings", async (req, res) => {
+//   const newListing=new Listing(req.body.listing);
+//   await newListing.save();
+//   res.redirect("/listings");
+//   // 2.
+//   // const listingData = new Listing(req.body.listing);
+//   // listingData.image = {
+//   //   filename: "listingimage",
+//   //   url: listingData.imageUrl
+//   // };
+//   // delete listingData.imageUrl;
+//   // const newListing = new Listing(listingData);
+//   // await newListing.save();
+//   // res.redirect("/listings");
+// });
+
+app.post("/listings", wrapAsync(async (req, res,next) => {
+   if(!req.body.listing){
+    throw new ExpressError(400,"Send Valid data for listing");
+   }
+    const { title, description, price, location, country, image } = req.body.listing;
+    
+    // if (!title || !description || !price || !location || !country || !image) {
+    //   return next(new ExpressError(400, "Page Not Found!"));
+    // }
+
+    const newListing = new Listing({
+      title,
+      description,
+      price,
+      location,
+      country,
+      image: {
+        url: image.url, // Ensure you're passing the object structure
+        filename: image.filename ||"listingimage", // Default filename (optional)
+      }
+    });
+    
+    await newListing.save();
+    res.redirect("/listings"); // Or wherever you want to redirect
+  
+}));
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit",  wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id",  wrapAsync(async (req, res) => {
+  if(!req.body.listing){
+    throw new ExpressError(400,"Send Valid data for listing");
+   }
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", wrapAsync( async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
   res.redirect("/listings");
-});
+}));
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
@@ -100,6 +135,20 @@ app.delete("/listings/:id", async (req, res) => {
 //   console.log("sample was saved");
 //   res.send("successful testing");
 // });
+
+// app.all("*",(req,res,next)=>{
+//   next( new ExpressError(404,"Page Not Found!"));
+// });
+app.use((req,res,next)=>{
+  next(new ExpressError(404,"Page Not Found!"));
+})
+
+app.use((err,req,res,next)=>{
+  let {statusCode=500,message="Something went wrong"}=err;
+  res.status(statusCode).send(message);
+});
+
+
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,29 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 
+
+const imageSchema = new Schema({
+  filename: {
+    type: String,
+    default: "listingimage",
+  },
+  url: {
+    type: String,
+    default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        : v,
+  },
+});
+// const imageSchema = new mongoose.Schema({
+//   url: {
+//     type: String,
+//     required: true,
+//   },
+//   filename: String,
+// });
+
 const listingSchema =new Schema({
     title:{
       type:String,
@@ -8,17 +31,21 @@ const listingSchema =new Schema({
     },
     description:String,
     image:{
-        filename:{
-          type:String,
-          default:"listingimage",
-        },
-        url:{
-
-       type:String,
-        default:"https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-        set: (v) => v==="" ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60":v,
-        }
+      type:imageSchema,
+      default: () => ({}),
     },
+    // image:{
+    //     filename:{
+    //       type:String,
+    //       default:"listingimage",
+    //     },
+    //     url:{
+
+    //    type:String,
+    //     default:"https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    //     set: (v) => v==="" ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60":v,
+    //     }
+    // },
     // image: {
     //   type: String,
     //   default:
